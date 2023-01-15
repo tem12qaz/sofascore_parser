0d095285d01229f0d745c66ac231d7d904502634
tem12qaz <tem12qaz@yandex.ru>
2023-01-15 16:14:10 +0300

    py310

--- request.py
+++ request.py
@@ -64,8 +64,8 @@ class Request:
                     resp = await session.get(
                         url=url,
                         headers=HEADERS,
-                        proxy='http://45.10.250.252:8000/',
-                        proxy_auth=proxy_auth
+                        proxy='http://RsxBk6:VvyEC@45.10.250.252:8000/',
+                        # proxy_auth=proxy_auth
                     )
                     resp = await resp.json()
 
