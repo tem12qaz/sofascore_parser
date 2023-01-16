6bf021f29eba4d57274a156939cd0b07a4bb3ecf
tem12qaz <tem12qaz@yandex.ru>
2023-01-16 20:06:19 +0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from parser import Parser
 
 if __name__ == "__main__":
-    parser = Parser(last_day_str='2023-01-10')
+    parser = Parser()
     parser.set_tennis()
     parser.init_workbook()
     parser.run()
