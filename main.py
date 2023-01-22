9b7d8999199ea024fbbef872b15e009a9948f30b
tem12qaz <tem12qaz@yandex.ru>
2023-01-23 01:29:40 +0300

    fix

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from parser import Parser
 if __name__ == "__main__":
     parser = Parser()
     parser.set_football()
-    # parser.run(parser.refresh_coefficients())
-    parser.run(parser.run_write())
+    parser.run(parser.run(parser.write()))
 
