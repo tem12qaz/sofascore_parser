b5c8e07cc024f56aa6bc1b057bc596ba309cb741
tem12qaz <tem12qaz@yandex.ru>
2023-01-23 00:08:40 +0300

    fix

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from parser import Parser
 if __name__ == "__main__":
     parser = Parser()
     parser.set_tennis()
-    parser.run(parser.refresh_coefficients())
+    parser.run(parser.add_1_to_coefficient())
